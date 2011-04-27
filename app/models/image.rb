c2d3e8e9dc3b4642cadc7a5f7b56c6203d380c0f
Prajakta Aher <prajakta.aher12@gmail.com>
2011-04-27 12:12:26 +0530

    added image model

---
+++ app/models/image.rb
@@ -0,0 +1,12 @@
+class Image < ActiveRecord::Base
+	belongs_to :user
+	
+	has_attached_file :photo, :styles => { :thumb=> "100x100#", :small  => "150x150>", :medium => "300x300>", :large =>   "400x400>" },
+                  :url  => "/assets_path/users/:id/:style/:basename.:extension",
+                  :path => ":rails_root/public/assets_path/users/:id/:style/:basename.:extension"
+					#:path => "d:/assets_path/users/:id/:style/:basename.:extension"
+	#validates_attachment_presence :photo
+	#validates_attachment_size :photo, :less_than => 5.megabytes
+	validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
+	
+end
