173b283c5277eeb347b13ea5c0a14dc9dee6b147
Harsha <harsha.sawant@harbingergroup.com>
2011-04-29 10:39:46 +0530

    modified audit log message

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -102,7 +102,8 @@ class TicketsController < ApplicationController
 	def assign
 		#   raise 'here '
 		@ticket = Ticket.find(params[:ticket_id])
-		@ticket.audit_comment = " #{current_user.full_name} assigned ticket"
+    user = User.find(params[:user_id])
+		@ticket.audit_comment = " #{current_user.full_name} assigned ticket to #{user.full_name}"
     @ticket.assign_to_engineer(params[:user_id])
 		@user = @ticket.assigned
 		if @ticket.save
